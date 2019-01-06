03d950b31f82c1fee509e987750ad517876d02cb
sarthak7295 <sarthak.rawat@yahoo.com>
2019-01-06 14:03:46 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongo connected"))
+  .catch(err => console.log(err));
+
+  
+app.get("/", (req, res) => res.send("hell!"));
+
+var port = process.env.port || 5000;
+
+app.listen(port, () => console.log("server running on port " + port));
